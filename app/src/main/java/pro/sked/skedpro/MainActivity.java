61581aabc92517f245472f26f17eec5ac6700220
Igor Bondar <bondint6@gmail.com>
2016-10-08 01:57:36 +0300

    Edit Gradle. Add GSON

--- app/src/main/java/pro/sked/skedpro/MainActivity.java
+++ app/src/main/java/pro/sked/skedpro/MainActivity.java
@@ -1,23 +1,36 @@
 package pro.sked.skedpro;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
+
 
 public class MainActivity extends Activity {
 
     ArrayList<ItemLst> items = new ArrayList<ItemLst>();
    ShopListAdapter shopListAdapter;
+    Date d = new Date();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // создаем адаптер
         fillItems();
 
@@ -28,10 +41,12 @@ public class MainActivity extends Activity {
 
     }
 
+
+
     // генерируем данные для адаптера
     void fillItems() {
         for (int i = 1; i <= 20; i++) {
-            items.add(new ItemLst("Item "  + i,"11/11/11 + " + i ,"Bondint "+i , i * 10));
+            items.add(new ItemLst("Item "  + i, d.toString() ,"Bondint "+i , i * 10));
 
          }
     }
--- app/src/main/java/pro/sked/skedpro/ShopListAdapter.java
+++ app/src/main/java/pro/sked/skedpro/ShopListAdapter.java
@@ -6,10 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
-import java.sql.Date;
 import java.util.ArrayList;
-import java.util.zip.Inflater;
+
 
 /**
  * Created by Bondint on 07.10.2016.
