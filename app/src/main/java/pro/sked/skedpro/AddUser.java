4ce4069fe7b1b9c510e0ad709f90616ff987ab38
Igor Bondar <bondint6@gmail.com>
2016-10-24 04:40:27 +0300

    Add Retrofit, GSON

--- app/src/main/java/pro/sked/skedpro/AddUser.java
+++ app/src/main/java/pro/sked/skedpro/AddUser.java
@@ -11,11 +11,11 @@ import android.widget.EditText;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -38,13 +38,13 @@ public class AddUser extends Activity {
 
     private Gson gson = new GsonBuilder().create();
     private Retrofit retrofit = new Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(URL)
             .build();
 
     private Link intf = retrofit.create(Link.class);
 
-    //private static String url_create_user = "http://sked.pro";
+
 
     //private static final String TAG_SUCCESS = "success";
 
@@ -65,34 +65,24 @@ public class AddUser extends Activity {
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 Map<String, String> mapJson = new HashMap<String, String>();
                 mapJson.put("login", Login.getText().toString());
                 mapJson.put("password", Password.getText().toString());
-                mapJson.put("email=", Email.getText().toString());
-                mapJson.put("name=", "Bondint_Test");
+                mapJson.put("email", Email.getText().toString());
+                mapJson.put("name", "");
 
-
-
-
-
-/*
                 Call<Object> call = intf.CreateUser(mapJson);
                 call.enqueue(new Callback<Object>() {
                     @Override
                     public void onResponse(Call<Object> call, Response<Object> response) {
-                        
+                    // Get result  from response.body()
                     }
-
                     @Override
                     public void onFailure(Call<Object> call, Throwable t) {
                         
                     }
                 });
-
-
-*/
+/*
                 Call<Object> call = intf.CreateUser(mapJson);
 
                 try {
@@ -107,11 +97,7 @@ public class AddUser extends Activity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
-               /* Response r = intf.CreateUser("login="+Login.getText().toString(), "password="+Password.getText().toString(), "email="+Email.getText().toString(), "name=Bondint");
-                String sss = gson.fromJson(r.body().toString(),String.class);
-                System.out.println(sss); */
+            */
             }
         });
 
--- app/src/main/java/pro/sked/skedpro/Link.java
+++ app/src/main/java/pro/sked/skedpro/Link.java
@@ -2,8 +2,8 @@ package pro.sked.skedpro;
 
 import java.util.Map;
 
-import retrofit.http.FieldMap;
 import retrofit2.Call;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
@@ -15,7 +15,8 @@ public interface Link {
 
     @FormUrlEncoded
     //@GET("get_user.php")
-    @POST("create_user.php")
-    Call<Object> CreateUser (@FieldMap Map <String, String> map);
+    @POST("/create_user.php")
+    Call<Object> CreateUser (@FieldMap Map<String, String> map);
+   // Call<Object> CreateUser(@Field("login") String login, @Field("password") String password, @Field("email") String email, @Field("name") String name);
 
 }
